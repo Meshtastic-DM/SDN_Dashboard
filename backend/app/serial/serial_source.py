94201156e95f32281c8c06c669f785eb6732b6fd
Tharoosha <lakshantharoosha@gmail.com>
2026-03-04 01:23:06 +0530

    added TODO comments

--- backend/app/serial/serial_source.py
+++ backend/app/serial/serial_source.py
@@ -1,3 +1,4 @@
+#TODO: This need to be deleted when the Meshtastic client is fully integrated and tested, as the client will handle parsing internally and emit structured events instead of raw lines. @Tharoosha
 import time
 import serial
 
